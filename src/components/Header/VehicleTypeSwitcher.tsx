4a7ff9d72c335013ef3928dcf1b5a48ce2e800f6
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-04-12 16:10:49 +0200

    Add vehicle type switching logic

--- src/components/Header/VehicleTypeSwitcher.tsx
+++ src/components/Header/VehicleTypeSwitcher.tsx
@@ -1,8 +1,19 @@
+'use client';
+
+import { useVehicleConfig } from '@/providers/VehicleConfigProvider';
 import { SegmentGroup } from '@chakra-ui/react';
 
 const VehicleTypeSwitcher = () => {
+  const setConfig = useVehicleConfig()[1];
   return (
-    <SegmentGroup.Root defaultValue={'C'} size="xs">
+    <SegmentGroup.Root
+      defaultValue={'C'}
+      size="xs"
+      onValueChange={({ value }) => {
+        if (value === null) return;
+        setConfig((prev) => ({ ...prev, vehicleType: value }));
+      }}
+    >
       <SegmentGroup.Indicator />
       <SegmentGroup.Items className="cursor-pointer" items={['B', 'C', 'X', 'N', 'L', 'DSC']} />
     </SegmentGroup.Root>
