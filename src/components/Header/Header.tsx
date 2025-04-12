0bb948c8d88c2f18e56404770309803cdcaca775
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-04-12 16:24:10 +0200

    Fix position sticky for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,14 +5,12 @@ import { VehicleTypeSwitcher } from './VehicleTypeSwitcher';
 
 const Header = () => {
   return (
-    <Theme appearance="dark">
-      <header className="sticky top-0 z-20 ">
-        <Container maxW="3xl" className="h-12 flex gap-4 items-center justify-end">
-          <VehicleTypeSwitcher />
-          <LocaleSwitcher />
-          <ColorModeButton />
-        </Container>
-      </header>
+    <Theme as="header" appearance="dark" className="sticky top-0 z-20">
+      <Container maxW="3xl" className="h-12 flex gap-4 items-center justify-end">
+        <VehicleTypeSwitcher />
+        <LocaleSwitcher />
+        <ColorModeButton />
+      </Container>
     </Theme>
   );
 };
