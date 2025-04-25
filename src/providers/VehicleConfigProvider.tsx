147c904964b766b21432c73c7c8dc95aaa0884b2
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-04-25 20:43:08 +0200

    Add further locales

--- src/providers/VehicleConfigProvider.tsx
+++ src/providers/VehicleConfigProvider.tsx
@@ -21,7 +21,7 @@ const VehicleConfigContext = createContext<
 const VehicleConfigProvider = ({ children }: PropsWithChildren) => {
   const locale = useLocale();
   const state = useState<VehicleConfig>({
-    culture: locale === 'de' ? 'de-DE' : 'en-US',
+    culture: localePerCulture[locale] ?? 'en-US',
     userType: 'P',
     vehicleType: 'C',
   });
@@ -33,6 +33,14 @@ const useVehicleConfig = () => {
   const context = useContext(VehicleConfigContext);
   if (!context) throw new Error('useVehicleConfig must be used within a VehicleConfigProvider');
   return context;
+};
+
+const localePerCulture: Record<string, string> = {
+  'de-DE': 'de',
+  'es-ES': 'es',
+  'fr-FR': 'fr',
+  'it-IT': 'it',
+  'nl-NL': 'nl',
 };
 
 export { useVehicleConfig, VehicleConfigProvider, type VehicleConfig };
