3a0db796afb9051756cd13aa7e6181992ee081c6
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-05-09 15:19:48 +0200

    Add further test case

--- src/__tests__/components/ComplexForm/schema.test.tsx
+++ src/__tests__/components/ComplexForm/schema.test.tsx
@@ -1,8 +1,10 @@
 import { ComplexForm } from '@/components/ComplexForm/ComplexForm';
 import en from '@/messages/en.json';
 import { withProviders } from '@/vitest.setup';
-import { render, waitFor, within } from '@testing-library/react';
+import { fireEvent, render, waitFor, within } from '@testing-library/react';
 import { describe, it } from 'vitest';
+
+const t = en.ComplexForm;
 
 describe('ComplexForm > Validation', () => {
   it('should trigger validation on submit', async () => {
@@ -11,7 +13,7 @@ describe('ComplexForm > Validation', () => {
     const errorsBefore = queryAllByRole('alert').length;
 
     // Act
-    await waitFor(() => getByRole('button', { name: en.ComplexForm.submit }).click());
+    await waitFor(() => getByRole('button', { name: t.submit }).click());
 
     // Assert
     expect(errorsBefore).toBe(0);
@@ -23,18 +25,34 @@ describe('ComplexForm > Validation', () => {
       it('should not be empty', async () => {
         // Arrange
         const { getByRole } = render(withProviders(<ComplexForm />));
-        const input = getByRole('combobox', { name: en.ComplexForm.vehicleData.make });
+        const input = getByRole('combobox', { name: t.vehicleData.make }) as HTMLSelectElement;
         const field = input.closest('[data-part="root"]') as HTMLElement;
         const errorBefore = within(field).queryByRole('alert');
 
         // Act
-        await waitFor(() => getByRole('button', { name: en.ComplexForm.submit }).click());
+        await waitFor(() => getByRole('button', { name: t.submit }).click());
 
         // Assert
         const errorAfter = within(field).queryByRole('alert');
         expect(errorBefore).toBeFalsy();
         expect(errorAfter).toBeTruthy();
         expect(errorAfter).toHaveTextContent(en.zod.invalid_type_received_undefined);
+      });
+
+      it('should not show an error if the value is correct', async () => {
+        // Arrange
+        const { getByRole } = render(withProviders(<ComplexForm />));
+        const input = getByRole('combobox', { name: t.vehicleData.make }) as HTMLSelectElement;
+        const field = input.closest('[data-part="root"]') as HTMLElement;
+        const errorBefore = within(field).queryByRole('alert');
+
+        // Act
+        fireEvent.change(input, { target: { value: [...input.options][1].value } });
+        await waitFor(() => getByRole('button', { name: t.submit }).click());
+
+        // Assert
+        expect(errorBefore).toBeFalsy();
+        expect(within(field).queryByRole('alert')).toBeFalsy();
       });
     });
   });
