f8cb0a81dbc84a6a4c570f70554d514204d30254
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-05-09 12:58:56 +0200

    Configure vitest

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import react from '@vitejs/plugin-react';
+import { loadEnv } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    env: loadEnv('', process.cwd(), ''),
+    setupFiles: ['./vitest.setup.ts'],
+    resolveSnapshotPath: (testPath) => `${testPath}.snap`,
+    server: {
+      deps: {
+        inline: ['next-intl'], // https://github.com/vercel/next.js/issues/77200
+      },
+    },
+  },
+});
